fac5bd687868650f47aa7246977a74b9d85adae7
mcpicornell <marc.castro.picornell@gmail.com>
2023-02-27 13:46:08 +0100

    ejercicio4

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ mediaButton.onclick = function () {
     mainListDiv.classList.toggle("show_list");
     mediaButton.classList.toggle("active");
 }
+
+//Dialog
+
+const showButton = document.getElementById('openForm');
+const dialogForm = document.getElementById('dialog-form');
+const outputBox = document.querySelector('output');
+
+// "Update details" button opens the <dialog> modally
+showButton.addEventListener('click', () => {
+    dialogForm.showModal();
+});
+
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+dialogForm.addEventListener('close', () => {
+  return outputBox.value = dialogForm.returnValue;
+});
